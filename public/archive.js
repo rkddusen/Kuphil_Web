7acc122fd5efa97b4085a5970a8d4575262378bf
Choiheeju59 <edouardlalo@naver.com>
2022-02-13 23:50:05 +0900

    add archive.js 0213

--- public/archive.js
+++ public/archive.js
@@ -1,11 +1,32 @@
+let showfirst = document.getElementsByClassName("posterbox_1")[0];
+let showsecond = document.getElementsByClassName("posterbox_2")[0];
+let buildOne = ``;
+let buildTwo = ``;
+
+
 function prevPage() {
     
 };
 function nextPage() {
 
 };
-    let number = 1;
 
 function showPage(){
+    let arc = [];
+    for (let i = 0; i < 25; i++) {
+        arc[i] = 'poster_' + i + '.png';
+    };
 
-}
+    buildOne += '<div class="poster_img" id = "firstposter">' + '<img src='  + arc[0] + ' alt="제n회포스터" .>' + '<div class="poster_title_text">제1회 정기 연주회</div></div>';
+    buildOne += '<div class="poster_img" id = "secondposter">' + '<img src='  + arc[1] + ' alt="제n회포스터" .>' + '<div class="poster_title_text">제2회 정기 연주회</div></div>';
+    buildOne += '<div class="poster_img" id = "thirdposter">' + '<img src='  + arc[2] + ' alt="제n회포스터" .>' + '<div class="poster_title_text">제3회 정기 연주회</div></div>';
+
+    buildTwo += '<div class="poster_img" id = "fourthposter">' + '<img src='  + arc[3] + ' alt="제n회포스터" .>' + '<div class="poster_title_text">제4회 정기 연주회</div></div>';
+    buildTwo += '<div class="poster_img" id = "fifthposter">' + '<img src='  + arc[4] + ' alt="제n회포스터" .>' + '<div class="poster_title_text">제5회 정기 연주회</div></div>';
+    buildTwo += '<div class="poster_img" id = "sixthposter">' + '<img src='  + arc[5] + ' alt="제n회포스터" .>' + '<div class="poster_title_text">제6회 정기 연주회</div></div>';
+
+    showfirst.innerHTML = buildOne;
+    showsecond.innerHTML = buildTwo;
+};
+
+showPage();
