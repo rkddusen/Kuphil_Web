f8c4e923efc3c295f6576e23c713ca8fb17d6f11
Choiheeju59 <edouardlalo@naver.com>
2022-02-15 21:23:18 +0900

    deleted mainpage // added archive (0215)

--- public/archive.js
+++ public/archive.js
@@ -1,53 +1,50 @@
-
 let showfirst = document.getElementsByClassName("posterbox_1")[0];
 let showsecond = document.getElementsByClassName("posterbox_2")[0];
-let buildOne = ``;
-let buildTwo = ``;
 let arc = [];
-let num = 0;
+let num = 1;
 
 function showPage(){
 //25 >> 전체 포스터의 수
-    for (let i = num; i < num + 6; i++) {
-        arc[i] = 'poster_' + i + '.png';
-    };
-
-    buildOne += '<div class="poster_img" id = "firstposter">' + '<img src='  + arc[0] + ' alt="정보가 없습니다." .>' + '<div class="poster_title_text">제' + arc[0]+ '회 정기 연주회</div></div>';
-    buildOne += '<div class="poster_img" id = "secondposter">' + '<img src='  + arc[1] + ' alt="정보가 없습니다." .>' + '<div class="poster_title_text">제' + arc[1] + '회 정기 연주회</div></div>';
-    buildOne += '<div class="poster_img" id = "thirdposter">' + '<img src='  + arc[2] + ' alt="정보가 없습니다." .>' + '<div class="poster_title_text">제' + arc[2]+ '회 정기 연주회</div></div>';
-    buildTwo += '<div class="poster_img" id = "fourthposter">' + '<img src='  + arc[3] + ' alt="정보가 없습니다." .>' + '<div class="poster_title_text">제' + arc[3]+ '회 정기 연주회</div></div>';
-    buildTwo += '<div class="poster_img" id = "fifthposter">' + '<img src='  + arc[4] + ' alt="정보가 없습니다." .>' + '<div class="poster_title_text">제' + arc[4]+ '회 정기 연주회</div></div>';
-    buildTwo += '<div class="poster_img" id = "sixthposter">' + '<img src='  + arc[5] + ' alt="정보가 없습니다." .>' + '<div class="poster_title_text">제' + arc[5]+ '회 정기 연주회</div></div>';
+    let buildOne = ``;
+    let buildTwo = ``;
+    for (i=0; i<3; i++){
+    buildOne += '<div class="poster_img" id = "poster_' + (num+i) + '">' + '<img src="poster_'  + (num+i) + '.png" alt="정보가 없습니다." .>' + '<div class="poster_title_text">제' + (num+i) + '회 정기 연주회</div></div>';
+    buildTwo += '<div class="poster_img" id = "poster_' + (num+i+3) + '">' + '<img src="poster_'  + (num+i+3) + '.png" alt="정보가 없습니다." .>' + '<div class="poster_title_text">제' + (num+i+3) + '회 정기 연주회</div></div>';
+    }
 
     showfirst.innerHTML = buildOne;
     showsecond.innerHTML = buildTwo;
 };
 
-showPage();
+
 
 function removePage() {
-    document.getElementsByClassName("posterbox_1")[0] = null;
-    document.getElementsByClassName("posterbox_2")[0] = null;
+    showfirst.innerHTML = null;
+    showsecond.innerHTML = null;
 }
 
 function prevPage() {
-    
+    removePage();
+    if (num >= 7) {
+        num -= 6;
+        showPage();
+    }
+    else {
+        alert("첫 페이지 입니다.");
+        showPage();
+    }
 };
+
 function nextPage() {
     removePage();
-    for (let i = num + 6; i < num + 12; i++) {
-        arc[i] = 'poster_' + i + '.png';
-    };
-
-    buildOne += '<div class="poster_img" id = "firstposter">' + '<img src='  + arc[0] + ' alt="정보가 없습니다." .>' + '<div class="poster_title_text">제' + i+ '회 정기 연주회</div></div>';
-    buildOne += '<div class="poster_img" id = "secondposter">' + '<img src='  + arc[1] + ' alt="정보가 없습니다." .>' + '<div class="poster_title_text">제' + i + '회 정기 연주회</div></div>';
-    buildOne += '<div class="poster_img" id = "thirdposter">' + '<img src='  + arc[2] + ' alt="정보가 없습니다." .>' + '<div class="poster_title_text">제' + i + '회 정기 연주회</div></div>';
-    buildTwo += '<div class="poster_img" id = "fourthposter">' + '<img src='  + arc[3] + ' alt="정보가 없습니다." .>' + '<div class="poster_title_text">제' + i + '회 정기 연주회</div></div>';
-    buildTwo += '<div class="poster_img" id = "fifthposter">' + '<img src='  + arc[4] + ' alt="정보가 없습니다." .>' + '<div class="poster_title_text">제' + i + '회 정기 연주회</div></div>';
-    buildTwo += '<div class="poster_img" id = "sixthposter">' + '<img src='  + arc[5] + ' alt="정보가 없습니다." .>' + '<div class="poster_title_text">제' + i + '회 정기 연주회</div></div>';
-
-    showfirst.innerHTML = buildOne;
-    showsecond.innerHTML = buildTwo;
-
+    if (num <= 18) {
+        num += 6;
+        showPage();
+    }
+    else {
+        alert("마지막 페이지 입니다.");
+        showPage();
+    }
 };
 
+showPage();
